0b472c0b5af8bee203a33b7365276d6c6e7b25d1
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2019-09-05 22:33:25 +0300

    feat(002): use ARRAY_BUFFER for coordinates

--- 002-egghead-course/index.js
+++ 002-egghead-course/index.js
@@ -4,7 +4,9 @@ class Main {
   gl = this.canvas.getContext('webgl');
 
   constructor(props) {
-    const { coords, pointSize, color } = props;
+    const { coords, pointSize, color, vertices, mode } = props;
+    this.mode = mode;
+    this.vertices = vertices;
 
     this._initGL();
     this._createShaders();
@@ -15,21 +17,21 @@ class Main {
     const { gl, canvas } = this;
 
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1, 1, 1, 1);
+    gl.clearColor(0, 0, 1, 1);
   }
 
   _createShaders() {
     const { gl } = this;
 
     const vertexSource = `
-    attribute vec4 coords;
-    attribute float pointSize;
+      attribute vec4 coords;
+      attribute float pointSize;
     
-    void main() {
-      gl_Position = coords;
-      gl_PointSize = pointSize;
-    }
-  `;
+      void main() {
+        gl_Position = coords;
+        gl_PointSize = pointSize;
+      }
+    `;
 
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexSource);
@@ -37,13 +39,13 @@ class Main {
 
     // mediump NOT medium
     const fragmentSource = `
-    precision mediump float;
-    uniform vec4 color;
-    
-    void main() {
-      gl_FragColor = color;
-    }
-  `;
+      precision mediump float;
+      uniform vec4 color;
+      
+      void main() {
+        gl_FragColor = color;
+      }
+    `;
 
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentSource);
@@ -59,11 +61,21 @@ class Main {
   }
 
   _createVertices(coords, pointSize, color) {
-    const { gl, shaderProgram } = this;
+    const { gl, shaderProgram, vertices } = this;
+
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(
+      gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     const coordsLocation = gl.getAttribLocation(shaderProgram, 'coords');
+    // gl.vertexAttrib3f(coordsLocation, ...coords);
+    gl.vertexAttribPointer(
+      coordsLocation, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coordsLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
     const pointSizeLocation = gl.getAttribLocation(shaderProgram, 'pointSize');
-    gl.vertexAttrib3f(coordsLocation, ...coords);
     gl.vertexAttrib1f(pointSizeLocation, pointSize);
 
     const colorLocation = gl.getUniformLocation(shaderProgram, 'color');
@@ -71,17 +83,23 @@ class Main {
   }
 
   draw() {
-    const { gl } = this;
+    const { gl, mode } = this;
 
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl[mode], 0, 3);
   }
 }
 
 const props = {
   coords: [0.2, 0, 0],
-  pointSize: 100.0,
-  color: [1, 0, 1, 1]
+  pointSize: 3.0,
+  color: [0.0, 1.0, 1.0, 1.0],
+  vertices: [
+    -0.9, -0.9, 0.0,
+    0.9, -0.9, 0.0,
+    0.0, 0.9, 0.0
+  ],
+  mode: 'TRIANGLES' // POINTS || LINES || TRIANGLES
 };
 const main = new Main(props);
 
