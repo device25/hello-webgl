72023c99dc5e5df297e2097b031605b9f6947496
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2019-09-05 22:44:33 +0300

    fix(002): use different mode and coordinates

--- 002-egghead-course/index.js
+++ 002-egghead-course/index.js
@@ -87,6 +87,7 @@ class Main {
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl[mode], 0, 3);
+    gl.drawArrays(gl[mode], 7, 3);
   }
 }
 
@@ -95,11 +96,23 @@ const props = {
   pointSize: 3.0,
   color: [0.0, 1.0, 1.0, 1.0],
   vertices: [
-    -0.9, -0.9, 0.0,
-    0.9, -0.9, 0.0,
-    0.0, 0.9, 0.0
+    -0.9, 0.5, 0,
+    -0.7, -0.5, 0,
+    -0.5, 0.5, 0,
+    -0.3, -0.5, 0,
+    -0.1, 0.5, 0,
+    0.1, -0.5, 0,
+    0.3, 0.5, 0,
+    0.5, -0.5, 0,
+    0.7, 0.5, 0,
+    0.9, -0.5, 0
   ],
-  mode: 'TRIANGLES' // POINTS || LINES || TRIANGLES
+  /**
+   * POINTS
+   * LINES || LINE_STRIP || LINE_LOOP
+   * TRIANGLES
+   * */
+  mode: 'LINE_STRIP'
 };
 const main = new Main(props);
 
