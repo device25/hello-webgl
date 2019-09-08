56f4d7741b9ab32d82a9c20d7ecc332d6bb6b79a
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2019-09-08 23:12:37 +0300

    feat(002): add rotation shader canvas

--- 002-egghead-course/index.js
+++ 002-egghead-course/index.js
@@ -148,3 +148,121 @@ for (let i = 0; i < props.vertexCount; i += 1) {
 const main = new Main(props);
 
 main.draw();
+
+// second example
+
+let gl;
+let shaderProgram;
+let vertices;
+let angle = 0;
+
+function initGL() {
+  const canvas2 = document.getElementById('canvas2');
+
+  gl = canvas2.getContext('webgl');
+  gl.viewport(0, 0, canvas2.width, canvas2.height);
+  gl.clearColor(1, 1, 1, 1);
+}
+
+function createShaders() {
+  const vertexSource = `
+      attribute vec4 coords;
+      attribute float pointSize;
+      uniform mat4 transformMatrix;
+      void main(void) {
+        gl_Position = transformMatrix * coords;
+        gl_PointSize = pointSize;
+      }
+    `;
+
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexSource);
+  gl.compileShader(vertexShader);
+
+  // mediump NOT medium
+  const fragmentSource = `
+      precision mediump float;
+      uniform vec4 color;
+      void main(void) {
+        gl_FragColor = color;
+      }
+    `;
+
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentSource);
+  gl.compileShader(fragmentShader);
+
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
+}
+
+function createVertices() {
+  vertices = [
+    -0.9, -0.9, 0.0,
+    0.9, -0.9, 0.0,
+    0.0, 0.9, 0.0
+  ];
+
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+  const coords = gl.getAttribLocation(shaderProgram, 'coords');
+//   gl.vertexAttrib3f(coords, 0.5, 0.5, 0);
+  gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(coords);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  const pointSize = gl.getAttribLocation(shaderProgram, 'pointSize');
+  gl.vertexAttrib1f(pointSize, 20);
+
+  const color = gl.getUniformLocation(shaderProgram, 'color');
+  gl.uniform4f(color, 0, 0, 0, 1);
+}
+
+function rotateY() {
+  const thisAngle = angle;
+  const cos = Math.cos(thisAngle);
+  const sin = Math.sin(thisAngle);
+  const matrix = new Float32Array(
+    [
+      cos, 0, sin, 0,
+      0, 1, 0, 0,
+      -sin, 0, cos, 0,
+      0, 0, 0, 1
+    ]);
+  const transformMatrix =
+    gl.getUniformLocation(shaderProgram, 'transformMatrix');
+  gl.uniformMatrix4fv(transformMatrix, false, matrix);
+}
+
+// function rotateZ() {
+//   const thisAngle = angle;
+//   const cos = Math.cos(thisAngle);
+//   const sin = Math.sin(thisAngle);
+//   const matrix = new Float32Array(
+//     [
+//       cos, sin, 0, 0,
+//       -sin, cos, 0, 0,
+//       0, 0, 1, 0,
+//       0, 0, 0, 1
+//     ]);
+//   const transformMatrix =
+//     gl.getUniformLocation(shaderProgram, 'transformMatrix');
+//   gl.uniformMatrix4fv(transformMatrix, false, matrix);
+// }
+
+function draw() {
+  rotateY(angle += 0.01);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  requestAnimationFrame(draw);
+}
+
+initGL();
+createShaders();
+createVertices();
+draw();
